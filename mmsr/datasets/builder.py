0ef0aa1e90692763d5263b1cf8ce117654bc9aaa
DESKTOP-JU60D4G\lizekun <imu.lizekun@gmail.com>
2021-11-28 11:10:09 +0800

    学习记录 model = MMDataParallel(model, device_ids=range(cfg.gpus)).cuda()

--- mmsr/datasets/builder.py
+++ mmsr/datasets/builder.py
@@ -1,10 +1,17 @@
 import copy
+import random
+from functools import partial
 
-from torch.utils.data import ConcatDataset
+import numpy as np
+import torch
+from torch.utils.data import ConcatDataset, DataLoader
 
+from lmmcv.parallel import collate
+from lmmcv.runner import get_dist_info
 from lmmcv.utils import build_from_cfg
 from .dataset_wrappers import RepeatDataset
 from .registry import DATASETS
+from .samplers import DistributedSampler
 
 
 def _concat_dataset(cfg, default_args=None):
@@ -59,3 +66,102 @@ def build_dataset(cfg, default_args=None):
         dataset = build_from_cfg(cfg, DATASETS, default_args)
 
     return dataset
+
+
+def build_dataloader(dataset,
+                     samples_per_gpu,
+                     workers_per_gpu,
+                     num_gpus=1,
+                     dist=True,
+                     shuffle=True,
+                     seed=None,
+                     drop_last=False,
+                     pin_memory=True,
+                     persistent_workers=True,
+                     **kwargs):
+    """Build PyTorch DataLoader.
+
+    In distributed training, each GPU/process has a dataloader.
+    In non-distributed training, there is only one dataloader for all GPUs.
+
+    Args:
+        dataset (:obj:`Dataset`): A PyTorch dataset.
+        samples_per_gpu (int): Number of samples on each GPU, i.e.,
+            batch size of each GPU.
+        workers_per_gpu (int): How many subprocesses to use for data
+            loading for each GPU.
+        num_gpus (int): Number of GPUs. Only used in non-distributed
+            training. Default: 1.
+        dist (bool): Distributed training/test or not. Default: True.
+        shuffle (bool): Whether to shuffle the data at every epoch.
+            Default: True.
+        seed (int | None): Seed to be used. Default: None.
+        drop_last (bool): Whether to drop the last incomplete batch in epoch.
+            Default: False
+        pin_memory (bool): Whether to use pin_memory in DataLoader.
+            Default: True
+        persistent_workers (bool): If True, the data loader will not shutdown
+            the worker processes after a dataset has been consumed once.
+            This allows to maintain the workers Dataset instances alive.
+            The argument also has effect in PyTorch>=1.7.0.
+            Default: True
+        kwargs (dict, optional): Any keyword argument to be used to initialize
+            DataLoader.
+
+    Returns:
+        DataLoader: A PyTorch dataloader.
+    """
+    rank, world_size = get_dist_info()
+    if dist:
+        sampler = DistributedSampler(
+            dataset,
+            world_size,
+            rank,
+            shuffle=shuffle,
+            samples_per_gpu=samples_per_gpu)
+        shuffle = False
+        batch_size = samples_per_gpu
+        num_workers = workers_per_gpu
+    else:
+        sampler = None
+        batch_size = num_gpus * samples_per_gpu
+        num_workers = num_gpus * workers_per_gpu
+
+    init_fn = partial(
+        worker_init_fn, num_workers=num_workers, rank=rank,
+        seed=seed) if seed is not None else None
+
+    if torch.__version__ >= '1.7.0':
+        kwargs['persistent_workers'] = persistent_workers
+
+    data_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=sampler,
+        num_workers=num_workers,
+        collate_fn=partial(collate, samples_per_gpu=samples_per_gpu),
+        pin_memory=pin_memory,
+        shuffle=shuffle,
+        worker_init_fn=init_fn,
+        drop_last=drop_last,
+        **kwargs)
+
+    return data_loader
+
+
+def worker_init_fn(worker_id, num_workers, rank, seed):
+    """Function to initialize each worker.
+
+    The seed of each worker equals to
+    ``num_worker * rank + worker_id + user_seed``.
+
+    Args:
+        worker_id (int): Id for each worker.
+        num_workers (int): Number of workers.
+        rank (int): Rank in distributed training.
+        seed (int): Random seed.
+    """
+
+    worker_seed = num_workers * rank + worker_id + seed
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
